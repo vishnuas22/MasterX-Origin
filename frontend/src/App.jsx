b0ad635dbd9e7be4691b38bbb4975624beb1cc9b
The Ghost <68109013+vishnuas22@users.noreply.github.com>
2025-04-26 19:44:35 +0530

    Add files via upload

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './index.css';
+
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Dashboard from './pages/Dashboard.jsx';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        {/* Redirect from / to /dashboard */}
+        <Route path="/" element={<Navigate to="/dashboard" />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
